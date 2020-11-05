3feb5622ca5d682553697f35d9c349ffab039c5d
Galen Emanuel <galen.emanuel.fencing@gmail.com>
2020-11-05 15:21:24 -0800

    initial styling of searchbar, icon added and relative position of elements within components placed

--- remoteroofing/src/components/SearchBar/SearchBar.js
+++ remoteroofing/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import axios from 'axios';
-
+import './SearchBar.css';
 
 const SearchBar = () => {
     return (
         <div className="searchBar">
-            <div className = "homeIconDiv"></div>
+            <div className = "homeIconDiv">
+                <img src="https://uploads-ssl.webflow.com/5f127ac8860f158e650f06b5/5f5a6d20aa3c71469c732751_blue-home-pin.png" alt="enter your address for free roof inspection">
+
+                </img>
+            </div>
             <input type="text" placeholder="Your Address"></input>
             <button>Get Inspection</button>
         </div>
